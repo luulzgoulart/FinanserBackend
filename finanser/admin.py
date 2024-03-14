a929e8f52836b605e7b943277e2af54ca7526171
Luiz Goulart <luulzgoulart@gmail.com>
2024-03-14 00:59:47 -0300

    Update admin.py

--- finanser/admin.py
+++ finanser/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from finanser.models import (UserData, Institution, Account, IncomeCategory, Income, ExpenseCategory, Expense)
 
-# Register your models here.
+admin.site.register(UserData)
+admin.site.register(Institution)
+admin.site.register(Account)
+admin.site.register(IncomeCategory)
+admin.site.register(ExpenseCategory)
+admin.site.register(Income)
+admin.site.register(Expense)
