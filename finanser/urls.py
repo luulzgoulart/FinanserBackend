cf8d506a7821dba86a7575c428b03e72e601306a
Luiz Goulart <luulzgoulart@gmail.com>
2024-03-15 00:57:02 -0300

    (FEAT) Create urls for auth views

--- finanser/urls.py
+++ finanser/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from finanser.views.views_auth import LoginView, LogoutView
 
-urlpatterns = []
+urlpatterns = [
+  path('login/', LoginView.as_view(), name='login'),
+  path('logout/', LogoutView.as_view(), name='logout'),
+]
